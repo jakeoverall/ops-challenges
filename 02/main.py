73ca7a28ca504d4b70eecb152948ab9a045a8bfa
Jake Overall <joverall22@gmail.com>
2020-10-07 20:00:21 -0600

    adds safety check for devices

--- 02/main.py
+++ 02/main.py
@@ -1,41 +1,54 @@
-#!/usr/bin/env python3
 import signal
 import sys
 import pingr
 import os
-from device import Device 
+from device import Device
 import banners
 
 devices = []
 
+
 def confirm_loop(message, action):
     print(message)
-    again = input("Y/n: ")
-    while again.capitalize() == "Y":
+    again = input("Y/n: ").capitalize()
+    while again == "Y":
         action()
         print(f"Continue: {message}?")
-        again = input("Y/n: ")
+        again = input("Y/n: ").capitalize()
+        again = "Y" if again != 'N' else "n"
+
 
 def create_system():
     print("ADDING SYSTEM TO MONITOR:")
     address = input("System Address: ")
     device = Device(address)
     devices.append(device)
-    confirm_loop("Do you want to recieve notifications if the system is down?", device.add_monitor)
+    confirm_loop(
+        "Do you want to recieve notifications if the system is down?", device.add_monitor)
+
 
 def main(args):
     os.system('cls')
     banners.print_main_banner()
     confirm_loop("Add a system for monitoring", create_system)
+    device_count = len(devices)
+    if device_count == 0:
+        quit()
+
+    print(f"MONITORING {device_count} DEVICES")
 
     for device in devices:
         pingr.add_device(device)
     pingr.start_monitoring()
 
-def signal_handler(signal, frame):
-    pingr.stop_monitoring()
+
+def quit():
     print(" Okay, Bye 👋 ")
     exit(0)
+
+def signal_handler(signal, frame):
+    pingr.stop_monitoring()
+    quit()
 
 
 signal.signal(signal.SIGINT, signal_handler)
--- 02/readme.md
+++ 02/readme.md
@@ -1,6 +1,36 @@
 Uptime Monitor
 =============
-# Title: ops-401d1: Challenge 01
+```python
+# Title: ops-401d1: Challenge 02
 # Author: Jake Overall
 # Date: 10/6/2020
-# Purpose: Ping Test with log writter
+# Purpose: System Uptime Monitor with logwritter and email notifications
+
+'''
+Welcome to:
+
+ ad88888ba                                         88b           d88                            
+d8"     "8b                                        888b         d888                            
+Y8,                                                88`8b       d8'88                            
+`Y8aaaaa,    8b       d8  ,adPPYba,                88 `8b     d8' 88   ,adPPYba,   8b,dPPYba,   
+  `"""""8b,  `8b     d8'  I8[    ""                88  `8b   d8'  88  a8"     "8a  88P'   `"8a  
+        `8b   `8b   d8'    `"Y8ba,                 88   `8b d8'   88  8b       d8  88       88  
+Y8a     a8P    `8b,d8'    aa    ]8I                88    `888'    88  "8a,   ,a8"  88       88  
+ "Y88888P"       Y88'     `"YbbdP"'                88     `8'     88   `"YbbdP"'   88       88  
+                 d8'                                                                            
+                d8'                  888888888888                                            
+'''
+```
+
+### Setup
+Add a `.env` file
+
+```.env
+email=YOUREMAIL
+password=EMAILPASSWORD
+```
+
+### Run
+```shell
+$ sudo python3 main.py
+```
