f625f813be0481d9f401ca55bb1d77d661082660
Jake Overall <joverall22@gmail.com>
2020-11-11 11:57:34 -0700

    use unique logger for various levels of logging

--- 09/main.py
+++ 09/main.py
@@ -2,40 +2,53 @@ import logging
 from logging import error
 from logging.handlers import RotatingFileHandler, TimedRotatingFileHandler
 
-logging.basicConfig(
-    handlers=[
-        RotatingFileHandler(
-            './s-log.log',
-            delay=False,
-            maxBytes=1000,
-            backupCount=3
-        ),
-        TimedRotatingFileHandler(
-            './t-log.log',
-            when='S',
-            interval=1,
-            backupCount=4,
-            delay=False
-        )],
-    level=logging.DEBUG,
-    format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
-    datefmt='%Y-%m-%dT%H:%M:%S')
+
+logger = logging.getLogger(__name__)
+
+
+def configure_logger():
+    logging.basicConfig(
+        handlers=[
+            RotatingFileHandler(
+                './s-log.log',
+                delay=False,
+                maxBytes=1000,
+                backupCount=3
+            ),
+            TimedRotatingFileHandler(
+                './t-log.log',
+                when='S',
+                interval=1,
+                backupCount=4,
+                delay=False
+            )],
+        level=logging.DEBUG,
+        format="[%(asctime)s] %(name)s || %(levelname)s || %(message)s",
+        datefmt='%Y-%m-%dT%H:%M:%S')
+    stream_handler = logging.StreamHandler()
+    file_handler = logging.FileHandler('errors.log')
+    file_handler.setFormatter(logging.Formatter('[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s'))
+    file_handler.setLevel(logging.ERROR)
+    logger.addHandler(stream_handler)
+    logger.addHandler(file_handler)
 
 
 def main():
-    for i in range(100000):
+    for i in range(1000):
         try:
             if(i % 3 == 0 and i % 5 == 0):
-                logging.critical(f"{i}-FIZZBUZZ 🌟")
+                logger.critical(f"{i}-FIZZBUZZ 🌟")
             elif(i % 3 == 0):
-                logging.info(f"{i}-FIZZ")
+                logger.info(f"{i}-FIZZ")
             elif(i % 5 == 0):
-                logging.warning(f"{i}-BUZZ")
+                logger.warning(f"{i}-BUZZ")
             else:
-                logging.debug(i)
+                logger.debug(i)
         except Exception as err:
-            logging.error(err)
+            logger.error(err)
+    logger.error("EVERYTHING WORKED...")
 
 
 if __name__ == "__main__":
+    configure_logger()
     main()
