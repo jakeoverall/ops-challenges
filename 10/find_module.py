d5359d110717b12f98196d3668378ee4c86b0be1
Jake Overall <joverall22@gmail.com>
2020-11-16 21:41:48 -0700

    find files

--- 10/find_module.py
+++ 10/find_module.py
@@ -1,8 +1,19 @@
-def find_file(filename, path):
-    print("test")
+import os
+import fnmatch
+
+
+def find_files(filename, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, filename):
+                match = os.path.join(root, name)
+                print(match)
+                result.append(match)
+    return result
 
 
 def main():
     filename = input("Filename: ")
-    path = input("Path: ")
-    find_file(filename, path)
+    path = input("Path: ") or "."
+    find_files(filename, path)
